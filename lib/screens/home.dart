0e64c84f9d50b0589ba0a09de91275e8ed4e3240
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 04:05:24 +0530

    alignment of the image

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,34 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}): super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: tdBGColor,
-        title: Row(children: [
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Icon(
             Icons.menu,
             color: tdBlack,
             size: 30,
           ),
           Container(
-        height: 40,
-        width: 40,
-        child: ClipRRect(
-          child: Image.asset('assets/images/avatar.jpeg'),
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              child: Image.asset('assets/images/avatar.jpeg'),
             ),
           ),
         ]),
       ),
-      
-        body: Container(
-          child: Text('This is home screen.'),
-
-        ),
+      body: Container(
+        child: Text('This is home screen.'),
+      ),
     );
-    
   }
 }
