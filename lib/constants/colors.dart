2c4e17ea467260b5331af68ce944906a4cb06a0e
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 03:17:09 +0530

    creating folders & files

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color.fromARGB(255, 64, 113, 218);
+
+const Color tdBlack = Color.fromARGB(84, 14, 1, 1);
+const Color tdGrey = Color.fromARGB(255, 161, 159, 159);
+
+const Color tdBGColor = Color.fromARGB(255, 251, 248, 248);
