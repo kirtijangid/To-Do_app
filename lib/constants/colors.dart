008c304f2625e02b07d813cf5142fcb457745131
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 04:50:36 +0530

    creating a container using white color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -6,4 +6,4 @@ const Color tdBlue = Color.fromARGB(255, 64, 113, 218);
 const Color tdBlack = Color.fromARGB(84, 14, 1, 1);
 const Color tdGrey = Color.fromARGB(255, 161, 159, 159);
 
-const Color tdBGColor = Color.fromARGB(255, 251, 248, 248);
+const Color tdBGColor = Color.fromARGB(255, 193, 191, 191);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,28 +7,42 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext) {
     return Scaffold(
-      appBar: AppBar(
         backgroundColor: tdBGColor,
-        title:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Icon(
-            Icons.menu,
-            color: tdBlack,
-            size: 30,
-          ),
-          Container(
-            height: 40,
-            width: 40,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/images/avatar.jpeg'),
+        appBar: _buildAppBar(),
+        body: Container(
+            child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/avatar.jpeg'),
           ),
-        ]),
-      ),
-      body: Container(
-        child: Text('This is home screen.'),
-      ),
+        ),
+      ]),
     );
   }
 }
