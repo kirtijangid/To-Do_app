60b1bed6cc7012e51e6ffcf1756e26c7040ef797
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 04:01:21 +0530

    creating menu icon, adding image & importing packages

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}); super(key: key);
+  const Home({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To-Do App')),
+        backgroundColor: tdBGColor,
+        title: Row(children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+        height: 40,
+        width: 40,
+        child: ClipRRect(
+          child: Image.asset('assets/images/avatar.jpeg'),
+            ),
+          ),
+        ]),
+      ),
+      
         body: Container(
           child: Text('This is home screen.'),
 
