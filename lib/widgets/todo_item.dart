f69b1928b3559811dda3a99c780372304efb870c
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 06:07:55 +0530

    adding delete icon

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -7,23 +7,39 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: ListTile(
-            onTap: () {},
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            tileColor: Colors.white,
-            leading: Icon(
-              Icons.check_box,
-              color: tdBlue,
-            ),
-            title: Text(
-              'Check Mail',
-              style: TextStyle(
-                fontSize: 16,
-                color: tdBlack,
-                decoration: TextDecoration.lineThrough,
-              ),
-            )));
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+        title: Text(
+          'Check Mail',
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+        trailing: Container(
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 22,
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
   }
 }
