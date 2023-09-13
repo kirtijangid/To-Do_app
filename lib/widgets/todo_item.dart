dd0e4c551de05a11ee2092e9d044fbcb7b611aa8
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 05:58:13 +0530

    adding another container with a checkbox icon and text in it

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class ToDoItem extends StatelessWidget {
   const ToDoItem({super.key});
@@ -6,7 +7,23 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('ToDo Item'),
-    );
+        child: ListTile(
+            onTap: () {},
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            tileColor: Colors.white,
+            leading: Icon(
+              Icons.check_box,
+              color: tdBlue,
+            ),
+            title: Text(
+              'Check Mail',
+              style: TextStyle(
+                fontSize: 16,
+                color: tdBlack,
+                decoration: TextDecoration.lineThrough,
+              ),
+            )));
   }
 }
