501af0533d54db342b551a010022a1454e79bd68
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-14 00:08:20 +0530

    change in colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color(0xFFDA4040);
-const Color tdBlue = Color.fromARGB(255, 64, 113, 218);
+const Color tdRed = Color.fromARGB(255, 236, 115, 115);
+const Color tdBlue = Color.fromARGB(255, 76, 114, 197);
 
-const Color tdBlack = Color.fromARGB(84, 14, 1, 1);
-const Color tdGrey = Color.fromARGB(255, 161, 159, 159);
+const Color tdBlack = Color.fromARGB(248, 10, 10, 10);
+const Color tdGrey = Color.fromARGB(255, 216, 210, 210);
 
-const Color tdBGColor = Color.fromARGB(255, 193, 191, 191);
+const Color tdBGColor = Color.fromARGB(255, 211, 209, 209);
