badbbb8cfdeaaa3bf9b2ac42058e172c102e4d10
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 12:19:51 +0530

    formatting

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -8,14 +8,26 @@ class ToDo {
     required this.todoText,
     this.isDone = false,
   });
-  static List<ToDo> todoList(){
+  static List<ToDo> todoList() {
     return [
       ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
       ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
-      ToDo(id: '03', todoText: 'Check Emails',),
-      ToDo(id: '04', todoText: 'Team Meetings',),
-      ToDo(id: '05', todoText: 'Work on mobile app',),
-      ToDo(id: '06', todoText: 'Dinner',),
-    ]
+      ToDo(
+        id: '03',
+        todoText: 'Check Emails',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'Team Meetings',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Work on mobile app',
+      ),
+      ToDo(
+        id: '06',
+        todoText: 'Dinner',
+      ),
+    ];
   }
 }
