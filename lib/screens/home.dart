96cb755c6ef5c7cf4ecbf732f07b56ba4331a552
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 23:39:34 +0530

    resolving _foundToDo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,7 +12,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
-  List<ToDo> _found = [];
+  List<ToDo> _foundToDo = [];
   final _todoController = TextEditingController();
 
   @override
@@ -28,75 +28,75 @@ class _HomeState extends State<Home> {
       appBar: _buildAppBar(),
       body: Stack(
         children: [
-        Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 15,
-          ),
-          child: Column(
-            children: [
-              searchBox(),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 50, bottom: 20),
-                      child: Text(
-                        'All To Dos',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w500,
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
+            ),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'All To Dos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                    for (ToDo todoo in _foundToDo.reversed)
-                      ToDoItem(
-                        todo: todoo,
-                        onToDoChanged: _handleToDoChange,
-                        onDeleteItem: _deleteToDoItem,
-                      ),
-                  ],
-                ),
-              )
-            ],
+                      for (ToDo todoo in _foundToDo.reversed)
+                        ToDoItem(
+                          todo: todoo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Row(children: [
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.only(
-                  bottom: 20,
-                  right: 20,
-                  left: 20,
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 5,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0.0, 0.0),
-                      blurRadius: 10.0,
-                      spreadRadius: 0.0,
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: InputDecoration(
+                      hintText: 'Add a new todo item',
+                      border: InputBorder.none,
                     ),
-                  ],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  controller: _todoController,
-                  decoration: InputDecoration(
-                    hintText: 'Add a new todo item',
-                    border: InputBorder.none,
                   ),
                 ),
               ),
-            ),
-            Container(
+              Container(
                 margin: EdgeInsets.only(
                   bottom: 20,
                   right: 20,
@@ -118,9 +118,9 @@ class _HomeState extends State<Home> {
                   ),
                 ),
               ),
-          ]),
-        ),
-      ],
+            ]),
+          ),
+        ],
       ),
     );
   }
