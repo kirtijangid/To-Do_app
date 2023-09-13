1ad2494a3e5603cb21b787b241f0fb35df309192
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 15:11:34 +0530

    creating a text bar at the bottom of the page to add items

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,7 @@ import 'package:todo_app/model/todo.dart';
 import 'package:todo_app/widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
-   Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
   final todosList = ToDo.todoList();
 
@@ -13,36 +13,70 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 15,
-        ),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 20),
-                    child: Text(
-                      'All To Dos',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+      body: Stack(children: [
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15,
+          ),
+          child: Column(
+            children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        'All To Dos',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-
-                  for( ToDo todo in todosList)
-                  ToDoItem(todo: todo,),
-                ],
+                    for (ToDo todo in todosList)
+                      ToDoItem(
+                        todo: todo,
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            margin: EdgeInsets.only(
+              bottom: 20,
+              right: 20,
+              left: 20,
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 5,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 0.0),
+                  spreadRadius: 0.0,
+                ),
+              ],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Add a new todo item',
+                border: InputBorder.none,
               ),
             ),
-          ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 
