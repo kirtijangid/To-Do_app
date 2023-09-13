41bb5a970de0a9901e5ce97adc50ec55f3ccd8d1
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-13 06:28:03 +0530

    creating a new dart file todo in model

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -7,6 +7,7 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {
           print('clicked on ToDo Item ');
